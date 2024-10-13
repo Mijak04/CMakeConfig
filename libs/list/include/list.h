4ddd19724683c085ecb15ce8ffe16222aea20e2c
Piotr Mijakowski <286937@student.pwr.edu.pl>
2024-10-13 16:06:49 +0200

    initial commit

---
+++ libs/list/include/list.h
@@ -0,0 +1,14 @@
+//
+// Created by piotr on 13.10.2024.
+//
+
+#pragma once
+
+#ifndef LIST_H
+#define LIST_H
+
+static int a;
+
+void test();
+
+#endif //LIST_H
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <list.h>
+
+int main(void)
+{
+    printf("Hello, World! %d \n", a);
+
+    test();
+
+    return 0;
+}
